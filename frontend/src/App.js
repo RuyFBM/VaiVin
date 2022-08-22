68c1cba47c58cfd8e72157b7ada31439858674fd
Ruy Monard <ruyfbm.dev@gmail.com>
2022-08-22 13:46:29 -0300

    VaiVin-01

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div>
+      <header >
+        <a href="/">VaiVin</a>
+      </header>
+      <main>
+        Lista de Produtos
+      </main>
+    </div>
+  );
+}
+
+export default App;
