e4054e176884e9909fecc85f4bbfeaf95c73132a
Ruy Monard <ruyfbm.dev@gmail.com>
2022-08-23 15:56:37 -0300

    VaiVin-Manage-State-By-Reducer-Hook

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,12 +1,53 @@
+import { useEffect, useReducer, useState } from "react";
 import { Link } from "react-router-dom";
-import data from "../data";
+import axios from 'axios';
+import logger from 'use-reducer-logger';
+// import data from "../data";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'FETCH_REQUEST':
+      return { ...state, loading: true };
+    case 'FETCH_SUCCESS':
+      return { ...state, products: action.payload, loading: false };
+    case 'FETCH_FAIL':
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+
+  }
+}
 
 function HomeScreen() {
-    return (
-      <div>
-        <h1>Produtos em destaque</h1>
-        <div className="products">
-          {data.products.map((product) => (
+  const [{ loading, error, products }, dispatch] = useReducer(logger(reducer), {
+    products: [],
+    loading: true, error: ''
+  });
+  // const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      dispatch({ type: 'FETCH_REQUEST' });
+      try {
+        const result = await axios.get('/api/products');
+        dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
+      } catch (err) {
+        dispatch({ type: 'FETCH_FAIL', payload: err.message });
+      }
+
+      // setProducts(result.data);
+    };
+    fetchData();
+  }, []);
+  return (
+    <div>
+      <h1>Featured Products</h1>
+      <div className="products">
+        {loading ? (
+          <div>Loading...</div>
+        ) : error ? (
+          <div>{error}</div>
+        ) : (
+          products.map((product) => (
             <div className="product" key={product.slug}>
               <Link to={`/product/${product.slug}`}>
                 <img src={product.image} alt={product.name} />
@@ -18,12 +59,13 @@ function HomeScreen() {
                 <p>
                   <strong>${product.price}</strong>
                 </p>
-                <button>Adicionar ao Carrinho</button>
+                <button>Adicionar ao carrinho</button>
               </div>
             </div>
-          ))}
-        </div>
+          ))
+        )}
       </div>
-    );
-  }
+    </div>
+  );
+}
 export default HomeScreen;
