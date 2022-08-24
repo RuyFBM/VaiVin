220b77af2d8884c334b6c8bd7267f6dd2684ae99
Ruy Monard <ruyfbm.dev@gmail.com>
2022-08-24 19:04:30 -0300

    VaiVin-Create-Product-Details-Screen

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -5,6 +5,7 @@ import logger from 'use-reducer-logger';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Product from '../components/Product';
+import { Helmet } from "react-helmet-async";
 // import data from "../data";
 
 const reducer = (state, action) => {
@@ -19,7 +20,7 @@ const reducer = (state, action) => {
       return state;
 
   }
-}
+};
 
 function HomeScreen() {
   const [{ loading, error, products }, dispatch] = useReducer(logger(reducer), {
@@ -43,6 +44,9 @@ function HomeScreen() {
   }, []);
   return (
     <div>
+      <Helmet>
+        <title>VaiVin</title>
+      </Helmet>
       <h1>Produtos em destaque</h1>
       <div className="products">
         {loading ? (
